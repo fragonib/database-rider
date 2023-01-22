0589a1d9a4b9182d4e763598f505d0df8e78a3fb
Francisco González <francisco.gonzalez@code.seat>
2023-01-22 20:33:06 +0100

    test: DBRider forces commit using ´@DataSet(executeScriptsBefore=)´ so Spring can´t roll back a TX

--- rider-examples/spring-boot-dbunit-sample/src/main/java/com/github/database/rider/springboot/model/user/UserRepository.java
+++ rider-examples/spring-boot-dbunit-sample/src/main/java/com/github/database/rider/springboot/model/user/UserRepository.java
@@ -2,10 +2,11 @@ package com.github.database.rider.springboot.model.user;
 
 import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 @Transactional
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
   /**
    *
---
+++ rider-examples/spring-boot-dbunit-sample/src/test/resources/scripts/users.sql
@@ -0,0 +1,6 @@
+INSERT INTO USERS (id, email, name)
+VALUES
+    (-1, 'dbunit@gmail.com', 'dbunit'),
+    (-2, 'rmpestano@gmail.com', 'rmpestano'),
+    (-3, 'springboot@gmail.com', 'springboot'),
+    (-4, 'junit5@gmail.com', 'junit5');
